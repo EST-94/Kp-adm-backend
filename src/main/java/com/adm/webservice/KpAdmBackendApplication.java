93f9670817931cf8840f9bcdb2de2c9c8b2ecf80
EST-94 <bxob1040@naver.com>
2023-10-21 22:16:16 +0900

    Initial commit

---
+++ src/main/java/com/adm/webservice/KpAdmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adm.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpAdmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpAdmBackendApplication.class, args);
+	}
+
+}
