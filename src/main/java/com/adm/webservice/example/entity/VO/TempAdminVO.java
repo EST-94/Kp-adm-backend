afcc95aa872eb9d163cf36659899a59ece9f6cc3
EST-94 <bxob1040@gmail.com>
2024-02-14 13:36:49 +0900

    add temp VO for admin (undefined object name yet)

---
+++ src/main/java/com/adm/webservice/example/entity/VO/TempAdminVO.java
@@ -0,0 +1,27 @@
+package com.adm.webservice.example.entity.VO;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record TempAdminVO(String uid, String userName) {
+
+    /*
+
+    TO PERFORM
+    - authority
+    - relations with admin service
+
+     */
+
+    private static List<TempAdminVO> adminVOList = Arrays.asList(
+            new TempAdminVO("AID-0000", "super"),
+            new TempAdminVO("AID-0001", "normalUser")
+    );
+
+    public static TempAdminVO getByUserName(String userName) {
+        return adminVOList.stream()
+                .filter(tempAdminVO -> tempAdminVO.userName.equals(userName))
+                .findFirst()
+                .orElse(null);
+    }
+}
